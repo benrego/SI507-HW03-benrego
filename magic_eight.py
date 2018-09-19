974db6c93f934d2acd0f10efee9d1ea9906529d0
benrego <jaminrego@gmail.com>
2018-09-19 18:20:45 -0400

    second branch commit

--- magic_eight.py
+++ magic_eight.py
@@ -3,6 +3,6 @@ def question():
     while user_input[-1] != "?":
         print("I'm sorry, I can only answer questions.")
         user_input = input("What is your question?: ")
-    return user_input
+    return "your question is: ", user_input
 
 first_pass = question()
