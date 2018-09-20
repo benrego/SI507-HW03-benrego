0ec2e099d36bf8eb8e89ef72b384b2aa2a82c47e
Steph <smecham@umich.edu>
2018-09-20 00:26:56 -0400

    Adding 8-ball responses

--- magic_eight.py
+++ magic_eight.py
@@ -1,5 +1,32 @@
+import random
+
 def question():
     user_input = input("What is your question?: ")
     return user_input
 
 first_pass = question()
+
+responses = [
+"It is certain.",
+"It is decidely so.",
+"Without a doubt.",
+"Yes - definitely.",
+"You may rely on it.",
+"As I see it, yes.",
+"Most likely.",
+"Outlook good.",
+"Yes.",
+"Signs point to yes.",
+"Reply hazy, try again.",
+"Ask again later.",
+"Better not tell you now.",
+"Cannot predict now.",
+"Concentrate and ask again.",
+"Don't count on it.",
+"My reply is no.",
+"My sources say no.",
+"Outlook not so good.",
+"Very doubtful."
+]
+
+print(random.choice(responses))
